de0c04e1a62b011179b972d241a0eb9f8a510879
RenanSouza <renansouza0122@gmail.com>
2024-04-06 15:32:23 -0300

    :sparkles: feat: stylization sign form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,44 @@
+import emailIcon from '/public/assets/icons/email-icon.svg';
+import eyeIcon from '/public/assets/icons/eye-icon.svg';
+import Image from 'next/image';
 import React from 'react'
 
 const signIn = () => {
   return (
-    <div>signIn</div>
+    <div className="signIn">
+
+      <form className="signIn_form">
+        <h2>Welcome</h2>
+
+        <div className="input_container">
+          <label htmlFor="">email</label>
+          <input type="email" name="email" id="email" />
+          <Image
+            src={emailIcon}
+            width={15}
+            height={15}
+            alt="email icon"
+          />
+        </div>
+        <div className="input_container">
+          <label htmlFor="">password</label>
+          <input type="password" name="password" id="password" />
+          <Image
+            src={eyeIcon}
+            width={15}
+            height={15}
+            alt="email icon"
+          />
+        </div>
+
+        <button>
+          sign in
+        </button>
+
+        <a href="#">Have an account</a>
+      </form>
+
+    </div>
   )
 }
 
