494b48ea579de57e9fef8ac2a623798ede3c3c16
RenanSouza <renansouza0122@gmail.com>
2024-04-20 01:02:41 -0300

    :sparkles: feat: add header

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./home.css"
+import { logoIcon } from '@/constants'
+import Image from 'next/image'
 
 const home = () => {
   return (
-    <div className=''>
-      Home
+    <div className='home'>
+      <header>
+        <nav>
+          <a href="#">index</a>
+          <a href="#">index</a>
+          <a className='logo' href="#">
+            <Image
+              src={logoIcon}
+              width={50}
+              height={50}
+              alt="logo image"
+            /></a>
+          <a href="#">index</a>
+          <a href="#">index</a>
+        </nav>
+      </header>
     </div>
   )
 }
--- constants/index.ts
+++ constants/index.ts
@@ -2,9 +2,10 @@ export const icons = {
     eye: '/assets/icons/eye-icon.svg',
     email: '/assets/icons/email-icon.svg',
     name: '/assets/icons/name-icon.svg',
+    logo:'/assets/icons/logo.svg'
 };
 
 export const eyeIcon = icons.eye;
 export const emailIcon = icons.email;
 export const nameIcon = icons.name;
-
+export const logoIcon = icons.logo;
