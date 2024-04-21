a018f50d7835f525c025c3c6ab7647bc7bffd84a
RenanSouza <renansouza0122@gmail.com>
2024-04-21 00:14:17 -0300

    :sparkles: feat: add home content

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -21,6 +21,16 @@ const home = () => {
           <a href="#">index</a>
         </nav>
       </header>
+
+      <div className="home_content">
+          <div className="home_image"></div>
+          <div className="home_circle"></div>
+          <div className="home_title">
+            <h2> U're in love</h2>
+            <h2>with/</h2>
+            <h3>Moda</h3>
+          </div>
+      </div>
     </div>
   )
 }
