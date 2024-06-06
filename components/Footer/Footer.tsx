3bf30c1a34a30574750b47fb715208bb689a330f
Renan Souza <renansouza0122@gmail.com>
2024-06-06 00:17:39 -0300

    :construction: footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,36 +1,37 @@
-import "./Footer.css"
-import logo from "@/public/assets/icons/black-logo.svg"
-import Image from 'next/image'
-export default function Footer(){
-    return(
-        <footer>
-            <div className="footer_logo">
-                <Image
-                src={logo}
-                width={50}
-                height={50}
-                alt="logo image"
-            />
-            </div>
-             
-            <div className="footer_informations">
-                <ul className="developers">
-                    <li>
-                        Development by <span>name</span>/
-                    </li>
-                    <li>
-                    <span>name</span>/
-                    </li>
-                    <li>
-                        <span>name</span>/
-                    </li>
-                    <li>
-                       <span>name</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="copy"> <h3>Copyright &copy; 2024</h3></div>
-           
-        </footer>
-    )
+import "./Footer.css";
+import logo from "@/public/assets/icons/black-logo.svg";
+import Image from "next/image";
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer_logo">
+        <Image src={logo} width={50} height={50} alt="logo image" />
+      </div>
+
+      <div className="footer_informations">
+        <ul>
+         <span>Explore</span>
+          <li><p>Login/</p></li>
+          <li><p>Male/</p></li>
+          <li><p>Female/</p></li>
+        </ul>
+        <ul>
+         <span>Team</span>
+         <li><p>name/</p></li>
+         <li><p>name/</p></li>
+         <li><p>name/</p></li>
+         <li><p>name/</p></li>
+        </ul>
+        <ul>
+            <span>Support</span>
+            <li>support@gmail.com</li>
+        </ul>
+      </div>
+      <div className="copy">
+        <h3>Copyright &copy;/ modaExpress</h3>
+        <a href="#">Back to top</a>
+        <p>Have a nice day 🎉</p>
+      </div>
+    </footer>
+  );
 }
